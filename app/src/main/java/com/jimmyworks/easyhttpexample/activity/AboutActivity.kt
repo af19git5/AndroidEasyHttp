2ca533d39591c3165228dd40c756b030fd2da236
Jimmy Kang <af19git5@gmail.com>
2022-08-18 16:45:53 +0800

    關於頁面跳轉github頁面錯誤修正

--- app/src/main/java/com/jimmyworks/easyhttpexample/activity/AboutActivity.kt
+++ app/src/main/java/com/jimmyworks/easyhttpexample/activity/AboutActivity.kt
@@ -1,11 +1,14 @@
 package com.jimmyworks.easyhttpexample.activity
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.jimmyworks.easyhttpexample.R
 import com.jimmyworks.easyhttpexample.databinding.ActivityAboutBinding
+import com.jimmyworks.easyhttpexample.utils.UiUtils
 
 
 class AboutActivity : AppCompatActivity(), View.OnClickListener {
@@ -25,9 +28,16 @@ class AboutActivity : AppCompatActivity(), View.OnClickListener {
                 finish()
             }
             binding.ivGithub.id -> {
-                val intent = Intent(Intent.ACTION_VIEW)
-                intent.data = Uri.parse("https://github.com/af19git5/AndroidEasyHttp")
-                startActivity(intent)
+                try {
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("https://github.com/af19git5/AndroidEasyHttp")
+                        )
+                    )
+                } catch (e: ActivityNotFoundException) {
+                    UiUtils.toast(this, getString(R.string.open_github_error))
+                }
             }
         }
     }
